6d0b7582e704d2823dc32cc8c29df698e68de715
aminederouich <medaminederouich@outlook.com>
2024-12-18 16:45:55 +0100

    update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,55 +1,38 @@
 module.exports = {
 	env: {
-		'node': true,
-		'browser': true,
-		'commonjs': true,
-		'es2021': true
+		node: true,
+		browser: true,
+		commonjs: true,
+		es2021: true
 	},
 	settings: {
 		react: {
-			'version': '18.0' // Remplace par la version exacte si connue
+			version: 'detect' // Laisse ESLint détecter la version
 		}
 	},
 	extends: [
 		'eslint:recommended',
 		'plugin:react/recommended'
 	],
-	overrides: [
-		{
-			'env': {
-				'node': true
-			},
-			'files': [
-				'.eslintrc.{js,cjs}'
-			],
-			'parserOptions': {
-				'sourceType': 'script'
-			}
-		}
-	],
 	parserOptions: {
-		'ecmaVersion': 'latest'
+		ecmaVersion: 2021, // Version explicite d'ECMAScript
+		sourceType: 'module', // Utile si vous utilisez des imports/exports
+		ecmaFeatures: {
+			jsx: true // Support pour JSX
+		}
 	},
 	plugins: [
 		'react'
 	],
 	rules: {
-		'indent': [
-			'error',
-			'tab'
-		],
-		'quotes': [
-			'error',
-			'single'
-		],
-		'semi': [
-			'error',
-			'never'
-		],
+		indent: ['error', 'tab'],
+		quotes: ['error', 'single'],
+		semi: ['error', 'never'],
 		'no-unused-vars': ['error', {
-			'vars': 'all',
-			'args': 'after-used',
-			'caughtErrors': 'all'
-		}]
+			vars: 'all',
+			args: 'after-used',
+			caughtErrors: 'all'
+		}],
+		'react/prop-types': 'off' // Désactive la vérification des PropTypes
 	}
 }
