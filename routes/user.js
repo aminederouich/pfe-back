8b1f5e5137aebd935e7027a60bf43ce2362c39ee
oumaima <oumaima.amdouni@esprit.tn>
2025-07-15 17:47:04 +0200

    feat: add user retrieval and ticket statistics routes

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { getAllUsers } = require('../controllers/user');
+const { getAllUsers, getUserByUid, getTicketStatsByUser } = require('../controllers/user');
 
 router.get('/getAllUsers', getAllUsers);
-
+router.get('/:uid', getUserByUid);
+router.get('/stats/:uid', getTicketStatsByUser);
 module.exports = router;
