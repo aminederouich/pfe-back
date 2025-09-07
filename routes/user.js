b876ac4aa3adacd79e8ee294ca8c4946d1a18a2b
Mohamed Amine DEROUICH <35892457+aminederouich@users.noreply.github.com>
2025-09-07 14:25:24 +0100

    feat: update calculateTicketScore to include assigneeId and modify route for user retrieval

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { getAllUsers, getUserByUid, getTicketStatsByUser, inviteNewEmployee, setPassword } = require('../controllers/user');
 
 router.get('/getAllUsers', getAllUsers);
-router.get('/:uid', getUserByUid);
+router.get('/getUserByUid/:uid', getUserByUid);
 router.get('/stats/:uid', getTicketStatsByUser);
 router.post('/invite-employee', inviteNewEmployee);
 router.post('/set-password', setPassword);
